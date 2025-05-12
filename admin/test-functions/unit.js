dd07ad5225a4b4b0b22382aff68178c155c2fec9
brandonkhor <156253994+brandonkhor@users.noreply.github.com>
2025-05-11 17:50:35 -0700

    Update unit.js

--- admin/test-functions/unit.js
+++ admin/test-functions/unit.js
@@ -1,4 +1,4 @@
 export function isPhoneNumber(phoneNumber) {
-  const phoneRegex = /((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}/;
+  const phoneRegex = /^((\(\d{3}\) ?)|(\d{3}[-. ]))?\d{3}[-. ]\d{4}$/;
   return phoneRegex.test(phoneNumber);
 }
