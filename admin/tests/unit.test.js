c7fea17c8db2e01b31de05326a9e8ac438142c5d
yozygao <yozygao@gmail.com>
2025-05-11 19:04:17 -0700

    Update unit.test.js

--- admin/tests/unit.test.js
+++ admin/tests/unit.test.js
@@ -1,7 +1,7 @@
 import { isPhoneNumber } from '../test-functions/unit';
-test('adds 1 + 2 to equal 4', () => {
-  expect(1 + 2).toBe(3);
-})
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(4)
+});
 // isPhoneNumber True
 test('Good phone number 1', () => {
   expect(isPhoneNumber('(510)111-1111')).toBe(true);
