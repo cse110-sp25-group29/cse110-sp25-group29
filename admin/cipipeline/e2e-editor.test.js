62211a0b8681a65268102e84847716dc9742cef7
brandonkhor <bkhor@ucsd.edu>
2025-06-05 01:12:13 -0700

    Changed main.yml to add e2e test and tweaked linter to accomodate testing files

--- admin/cipipeline/e2e-editor.test.js
+++ admin/cipipeline/e2e-editor.test.js
@@ -1,19 +1,15 @@
 describe('Basic user flow for Website', () => {
-  // First, visit the editor page
-  beforeAll(async () => {
+  beforeAll(async() => {
     await page.goto('https://cse110-sp25-group29.github.io/cse110-sp25-group29/source/assets/editor-page.html');
   });
 
-  // Each it() call is a separate test
-  // Here, we check to make sure that all 20 <product-item> elements have loaded
-  
-  it('Check if light theme works as expected', async () => {
+  it('Check if light theme works as expected', async() => {
     console.log('Checking for light theme...');
 
-    //Finds the class name of the body tag
+    // Finds the class name of the body tag
     const bodyClass = await page.$eval('body', (body) => {
-        return body.className;
+      return body.className;
     });
-    expect(bodyClass).toBe("");
+    expect(bodyClass).toBe('');
   });
 });
--- admin/cipipeline/e2e-homepage.test.js
+++ admin/cipipeline/e2e-homepage.test.js
@@ -1,41 +1,35 @@
 describe('Basic user flow for Website', () => {
-  // First, visit the homepage
-  beforeAll(async () => {
+  beforeAll(async() => {
     await page.goto('https://cse110-sp25-group29.github.io/project-homepage/homepage.html');
   });
 
-  // Each it() call is a separate test
-  // Here, we check to make sure that all 20 <product-item> elements have loaded
-  
-  it('Check if light theme works as expected', async () => {
+  it('Check if light theme works as expected', async() => {
     console.log('Checking for light theme...');
 
-    //Finds the class name of the body tag
     const bodyClass = await page.$eval('body', (body) => {
-        return body.className;
+      return body.className;
     });
-    expect(bodyClass).toBe("");
+    expect(bodyClass).toBe('');
   });
 
-  it('Check if toggle dark theme works as expected', async () => {
+  it('Check if toggle dark theme works as expected', async() => {
     console.log('Checking for dark theme...');
 
     const themeButton = await page.$('#theme-toggle');
     await themeButton.click();
 
-
     const bodyClass = await page.$eval('body', (body) => {
-        return body.className;
+      return body.className;
     });
-    expect(bodyClass).toBe("dark-theme");
+    expect(bodyClass).toBe('dark-theme');
   });
 
-  it('How it works link is correct', async () => {
+  it('How it works link is correct', async() => {
     const href = await page.$eval('.how-it-works a', el => el.href);
-    expect(href).toBe('https://cse110-sp25-group29.github.io/project-homepage/homepage.html'); 
+    expect(href).toBe('https://cse110-sp25-group29.github.io/project-homepage/homepage.html');
   });
 
-  it('GitHub link has correct href and opens in new tab', async () => {
+  it('GitHub link has correct href and opens in new tab', async() => {
     const link = await page.$('.github a');
     const href = await page.evaluate(el => el.href, link);
     const target = await page.evaluate(el => el.target, link);
@@ -44,7 +38,7 @@ describe('Basic user flow for Website', () => {
     expect(target).toBe('_blank');
   });
 
-  it('Our Team link has correct href and opens in new tab', async () => {
+  it('Our Team link has correct href and opens in new tab', async() => {
     const link = await page.$('.team a');
     const href = await page.evaluate(el => el.href, link);
     const target = await page.evaluate(el => el.target, link);
@@ -52,6 +46,4 @@ describe('Basic user flow for Website', () => {
     expect(href).toBe('https://cse110-sp25-group29.github.io/cse110-sp25-group29/admin/team.html');
     expect(target).toBe('_blank');
   });
-
-  
 });
