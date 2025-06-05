9bf614ba3d5aa45b07dd7fdd057ef501fdb51f6a
brandonkhor <bkhor@ucsd.edu>
2025-06-05 01:04:34 -0700

    Added more e2e tests for homepage

--- admin/cipipeline/e2e-homepage.test.js
+++ admin/cipipeline/e2e-homepage.test.js
@@ -29,4 +29,29 @@ describe('Basic user flow for Website', () => {
     });
     expect(bodyClass).toBe("dark-theme");
   });
+
+  it('How it works link is correct', async () => {
+    const href = await page.$eval('.how-it-works a', el => el.href);
+    expect(href).toBe('https://cse110-sp25-group29.github.io/project-homepage/homepage.html'); 
+  });
+
+  it('GitHub link has correct href and opens in new tab', async () => {
+    const link = await page.$('.github a');
+    const href = await page.evaluate(el => el.href, link);
+    const target = await page.evaluate(el => el.target, link);
+
+    expect(href).toBe('https://github.com/cse110-sp25-group29/cse110-sp25-group29');
+    expect(target).toBe('_blank');
+  });
+
+  it('Our Team link has correct href and opens in new tab', async () => {
+    const link = await page.$('.team a');
+    const href = await page.evaluate(el => el.href, link);
+    const target = await page.evaluate(el => el.target, link);
+
+    expect(href).toBe('https://cse110-sp25-group29.github.io/cse110-sp25-group29/admin/team.html');
+    expect(target).toBe('_blank');
+  });
+
+  
 });
